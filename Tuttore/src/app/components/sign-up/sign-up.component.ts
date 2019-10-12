2d306f84e09b635f8ee57a01221fbcb608b1d195
Saicod <acastillos@unal.edu.co>
2019-10-12 04:45:29 -0500

    Sign Up Model Implemented :construction:

--- Tuttore/src/app/components/sign-up/sign-up.component.ts
+++ Tuttore/src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentModel } from 'src/app/models/student.model';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent{
-  log(x) {console.log(x);}
+export class SignUpComponent implements OnInit{
+  newStudent: StudentModel;
+
+  ngOnInit() {
+    this.newStudent = new StudentModel();
+  }
 }
